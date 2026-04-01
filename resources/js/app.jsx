1b797090564052bee635331e46f233ac0750f441
Isneha Manandhar <isnehamanandhar88@gmail.com>
2026-04-01 10:58:37 +0545

    uptodate

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -6,7 +6,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import ScrollToTop from '@/Components/ScrollToTop';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME ;
 
 createInertiaApp({
     title: (title) => `${title} - शुचीखबर`,
